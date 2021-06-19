80ff3932de9b0fb8355d7385138350e8425e0036
12312312345678 <me33523@naver.com>
2021-06-19 15:33:13 +0900

    Update questions.js
    문제 추가

--- questions.js
+++ questions.js
@@ -1,82 +1,172 @@
 questions = [
     {
         "NAME": "모든 고등 종교는 비록 그 형태가 다를 지라도 인간이 보편적으로 공유하는 지고한 가치를 담고 있다.",
-        "value": 10
+        "FreedomValue": 10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "예수는 신적 존재로서가 아닌, 세상에 올바른 지고한 윤리를 제공한 사람으로 이해했을 때, 교회와 사회에 더 큰 영향력과 교훈을 행사할 수 있다.",
-        "value": 10
+        "FreedomValue": 10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "성경에 기록된 예수는 당시의 현실과 당시의 생각이 덧붙혀진 역사적 산물이다.",
-        "value": 10
+        "FreedomValue": 10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "할 수 있다면, 성경이 쓰여진 과거로 가서 어떤 과정으로 쓰여졌는지 확실하게 해야 성경을 더 잘 해석할 수 있다.",
-        "value": 10
+        "FreedomValue": 10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "예수의 사역은 죄를 용서하는 의미가 아닌 약하고 가난한 자들을 위해 봉사하고, 그것을 시범으로 보여주는 것에 더 중점을 맞추었다.",
-        "value": 10
+        "FreedomValue": 10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "예수의 선포는 '하나님이 곧 다스리실 것이다'로 요약할 수 있다.",
-        "value": 10
+        "FreedomValue": 10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "예수의 가르침을 가르치는 이유는 그의 가르침이 윤리적으로 타당하기 때문이다.",
-        "value": 10
+        "FreedomValue": 10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "하나님의 계시는 성경 속에서 발견할 수 있으며 더 올바른 형태와 진리를 찾아내기 위해 노력해야 한다.",
-        "value": 10
+        "FreedomValue": 10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "세상이 성경과 신학의 비판을 따라 변혁되어야 한다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "예수가 태어난 이유는 세상의 죄를 없애고 그의 목숨으로 화목제물로 바치기 위해서였다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "산상수훈은 철저하게 지키기 위해 주어졌으며, 그대로 지키는 것이 산상수훈의 가치를 따르는 것이다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "하이델베르크 교리문답, 웨스트민스터 신앙고백, 기독교 강요와 같은 르네상스 시기 종교개혁 문서들은 현제에도 사람들을 가르치기에 충분하다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "옛 교회 회의(니케아, 콘스탄티노플, 칼케돈 공의회)에서 논의했던 것은 그 논의와 결과만으로도 가치가 있고 현제 교회 생활과 신앙고백에도 반영되어야 한다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "성경에 기록된 계명들은 모두 그 자체로 가치와 의미가 있고 그래서 지켜야 한다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "성경은 하나님의 구원의 역사를 파악하는데 오류가 없는 유일무이한 책으로 더이상 추가될 수도 거기에서 제거될 수도 없다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "사도신경을 외우는 것은 신자들의 의무이다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "만약 (이중)예정론이 사실이라면, 어떤 사람들은 자신이 아무리 구원을 원하여도 구원에 이를 수 없을 것이다. 그러므로 거부되어야 한다.",
-        "value": +10
+        "FreedomValue": +10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "예정론은 신학 이론 이상의 가치가 있는 교훈이다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "인간이 전적으로 타락했다는 것은 인간의 도덕성과 종교성에 국한해 생각할 문제이다.",
-        "value": -10
+        "FreedomValue": -10,
+        "FaithfulValue": 0
     },
     {
         "NAME": "하나님의 구원은 인간을 넘어서 새로운 세상(천국)을 만들어 나가는 데 이르게 된다.",
-        "value": 10
+        "FreedomValue": 10,
+        "FaithfulValue": 0
+    },
+    {
+        "NAME": "모든 권능의 출처는 성령 하나님이다. 그러므로 은사를 받은 사람들에 의해 교회가 지도되어",
+        "FreedomValue": 0,
+        "FaithfulValue": 10
+    },
+    {
+        "NAME": "신학은 인문학과 같은 공공의 학문이다. 심지어 신앙이 없더라도 신학에 충분히 기여를 할 수 있으며 그 가치에 따를 수 있다.",
+        "FreedomValue": 0,
+        "FaithfulValue": -10
+    },
+    {
+        "NAME": "방언, 통역, 치유와 같은 현상은 교회를 새롭게 움직이는 동력이다.",
+        "FreedomValue": 0,
+        "FaithfulValue": 10
+    },
+    {
+        "NAME": "교회는 현대적인 사고와 이성적인 요소가 있어야 현대 사회에서 올바르게 살아가고 선포할 수 있다.",
+        "FreedomValue": 0,
+        "FaithfulValue": -10
+    },
+    {
+        "NAME": "성령의 권능이 없는 사람은 가짜 신앙인이다.",
+        "FreedomValue": 0,
+        "FaithfulValue": 10
+    },
+    {
+        "NAME": "성경에 기록된 기적과 귀신에 대한 기사는 그 때 당시의 사람들의 시대적 상황 속에서 형성된 것이다",
+        "FreedomValue": 0,
+        "FaithfulValue": -10
+    },
+    {
+        "NAME": "교회의 지도자들은 모두 신앙인으로, 하나님 혹은 절대적인 존재에 대한 경험이 있어야 한다.",
+        "FreedomValue": 0,
+        "FaithfulValue": 10
+    },
+    {
+        "NAME": "만약 교리나 신앙고백을 인정하고 따른다면, 그 사람을 교회의 지도자로 세우지 못할 이유는 없다",
+        "FreedomValue": 0,
+        "FaithfulValue": -10
+    },
+    {
+        "NAME": "신앙은 그의 행동, 특히 종교적 행동과 은사적 현상들로부터 검증되어야 한다.",
+        "FreedomValue": 0,
+        "FaithfulValue": 10
+    },
+    {
+        "NAME": "신앙인은 그 사람이 어떤 것을 말하고 생각하고 가르치는가로 이해할 수 있다.",
+        "FreedomValue": 0,
+        "FaithfulValue": -10
+    },
+    {
+        "NAME": "인간과 하나님을 연결해 주는 것이 교회의 역할이다.",
+        "FreedomValue": 0,
+        "FaithfulValue": 10
+    },
+    {
+        "NAME": "올바른 신학이 없다면 교회는 인문학 강습소 또는 몰이성적인 집단이 될 것이다.",
+        "FreedomValue": 0,
+        "FaithfulValue": -10
+    },
+    {
+        "NAME": "초월적인 존재에 대한 이끌림의 감정은 종교를 설명하는데 좋은 아이디어이다.",
+        "FreedomValue": 0,
+        "FaithfulValue": 10
+    },
+    {
+        "NAME": "종교란 당연히 그 이전에 종교를 설교했던 설교자들과 신학자들, 그리고 권세자들로부터 이어져 온 초국가적 기관이다.",
+        "FreedomValue": 0,
+        "FaithfulValue": -10
     }
 ];
