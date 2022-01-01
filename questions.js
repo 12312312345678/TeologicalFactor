27e1c17726350b874645c63120ddc4284bea3522
12312312345678 <me33523@naver.com>
2022-01-02 03:14:07 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -21,19 +21,19 @@ questions = [
 		"NAME": "성경이 쓰여진 과거로 가서 어떤 과정으로 쓰여졌는지 확실하게 해야 성경을 더 잘 해석할 수 있다.",
 		"FreedomValue": 10,
 		"FaithfulValue": 0,
-		"QID": "HM2",
+		"QID": "HM2"
 	},
 	{
 		"NAME": "예수의 사역은 죄를 용서하는 의미가 아닌 약하고 가난한 자들을 위해 봉사하고, 그것을 시범으로 보여주는 것에 더 중점을 맞추었다.",
 		"FreedomValue": 10,
 		"FaithfulValue": 0,  
-		"QID": "HM3",
+		"QID": "HM3"
 	},
 	{
 		"NAME": "예수의 선포는 '하나님이 곧 다스리실 것이다'로 요약할 수 있다.",
 		"FreedomValue": 10,
 		"FaithfulValue": 0,
-		"QID": "HM4",
+		"QID": "HM4"
 	},
 	{
 		"NAME": "예수의 가르침을 가르치는 이유는 그의 가르침이 윤리적으로 타당하기 때문이다.",
@@ -122,61 +122,61 @@ questions = [
 	{
 		"NAME": "모든 권능의 출처는 성령 하나님이다. 그러므로 은사를 받은 사람들에 의해 교회가 지도되어야 한다",
 		"FreedomValue": 0,
-		"FaithfulValue": 1,0
+		"FaithfulValue": 10,
 		"QID": "FP1"
 	},
 	{
 		"NAME": "신학은 인문학과 같은 공공의 학문이다. 심지어 신앙이 없더라도 신학에 충분히 기여를 할 수 있으며 그 가치에 따를 수 있다.",
 		"FreedomValue": 0,
-		"FaithfulValue": -,10
+		"FaithfulValue": -10
 		"QID": "FN1"
 	},
 	{
 		"NAME": "방언, 통역, 치유와 같은 현상은 교회를 새롭게 움직이는 동력이다.",
 		"FreedomValue": 0,
-		"FaithfulValue": 1,0,
+		"FaithfulValue": 10,
 		"QID": "FP2"
 	},
 	{
 		"NAME": "교회는 현대적인 사고와 이성적인 요소가 있어야 현대 사회에서 올바르게 살아가고 선포할 수 있다.",
 		"FreedomValue": 0,
-		"FaithfulValue": -,10,
+		"FaithfulValue": -10,
 		"QID": "PN2"
 	},
 	{
 		"NAME": "성령의 권능이 없는 사람은 가짜 신앙인이다.",
 		"FreedomValue": 0,
-		"FaithfulValue": 1,0,
+		"FaithfulValue": 10,
 		"QID": "FP3"
 	},
 	{
 		"NAME": "성경에 기록된 기적과 귀신에 대한 기사는 그 때 당시의 사람들의 시대적 상황 속에서 형성된 것이다",
 		"FreedomValue": 0,
-		"FaithfulValue": -,10,
+		"FaithfulValue": -10,
 		"QID": "FN3"
 	},
 	{
 		"NAME": "교회의 지도자들은 모두 신앙인으로, 하나님 혹은 절대적인 존재에 대한 경험이 있어야 한다.",
 		"FreedomValue": 0,
-		"FaithfulValue": 1,0,
+		"FaithfulValue": 10,
 		"QID": "FP4"
 	},
 	{
 		"NAME": "만약 교리나 신앙고백을 인정하고 따른다면, 그 사람을 교회의 지도자로 세우지 못할 이유는 없다",
 		"FreedomValue": 0,
-		"FaithfulValue": -,10,
+		"FaithfulValue": -10,
 		"QID": "F1"
 	},
 	{
 		"NAME": "신앙은 그의 행동, 특히 종교적 행동과 은사적 현상들로부터 검증되어야 한다.",
 		"FreedomValue": 0,
-		"FaithfulValue": 1,0,
+		"FaithfulValue": 10,
 		"QID": "FN4"
 	},
 	{
 		"NAME": "신앙인은 그 사람이 어떤 것을 말하고 생각하고 가르치는가로 이해할 수 있다.",
 		"FreedomValue": 0,
-		"FaithfulValue": -,10,
+		"FaithfulValue": -10,
 		"QID": "FP5"
 	},
 	{
@@ -188,13 +188,13 @@ questions = [
 	{
 		"NAME": "올바른 신학이 없다면 교회는 인문학 강습소 또는 몰이성적인 집단이 될 것이다.",
 		"FreedomValue": 0,
-		"FaithfulValue": -,10,
+		"FaithfulValue": -10,
 		"QID": "FP6"
 	},
 	{
 		"NAME": "초월적인 존재에 대한 이끌림의 감정은 종교를 설명하는데 좋은 아이디어이다.",
 		"FreedomValue": 0,
-		"FaithfulValue": 1,
+		"FaithfulValue": 10
 		"QID": "FN6"
 	},
 	{
@@ -218,9 +218,9 @@ questions = [
 		"QID": "NN1"
 	},
 	{
-        "NAME": "신학은 교회와 세상을 위한 교의학이다. 교의학을 기반으로 한 가르침이 교회에서 이루어져야 한다.",
-        "FreedomValue": -10,
-        "FaithfulValue": -10
+        	"NAME": "신학은 교회와 세상을 위한 교의학이다. 교의학을 기반으로 한 가르침이 교회에서 이루어져야 한다.",
+        	"FreedomValue": -10,
+       		"FaithfulValue": -10,
 		"QID": "FP8"
-    }
+    	}
 ];
